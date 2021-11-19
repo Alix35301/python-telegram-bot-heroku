6a2de280022ace5226c90ccac007da012f7e42a6
alix35301 <alix35301@gmail.com>
2021-11-19 11:38:45 +0500

    s'

--- bot.py
+++ bot.py
@@ -70,7 +70,6 @@ def check():
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
 
-    import info
 
     # make sure this path is correct
     PATH = os.environ.get("CHROMEDRIVER_PATH")
@@ -103,45 +102,45 @@ def check():
             # add to cart
             atcBtn.click()
 
-            # go to cart and begin checkout as guest
-            driver.get("https://www.bestbuy.com/cart")
+            # # go to cart and begin checkout as guest
+            # driver.get("https://www.bestbuy.com/cart")
 
-            checkoutBtn = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div/div[2]/div[1]/div/div/span/div/div[2]/div[1]/section[2]/div/div/div[3]/div/div[1]/button"))
-            )
-            checkoutBtn.click()
-            print("Successfully added to cart - beginning check out")
+            # checkoutBtn = WebDriverWait(driver, 10).until(
+            #     EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div/div[2]/div[1]/div/div/span/div/div[2]/div[1]/section[2]/div/div/div[3]/div/div[1]/button"))
+            # )
+            # checkoutBtn.click()
+            # print("Successfully added to cart - beginning check out")
 
-            # fill in email and password
-            emailField = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "fld-e"))
-            )
-            emailField.send_keys(info.email)
+            # # fill in email and password
+            # emailField = WebDriverWait(driver, 10).until(
+            #     EC.presence_of_element_located((By.ID, "fld-e"))
+            # )
+            # emailField.send_keys(info.email)
 
-            pwField = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "fld-p1"))
-            )
-            pwField.send_keys(info.password)
+            # pwField = WebDriverWait(driver, 10).until(
+            #     EC.presence_of_element_located((By.ID, "fld-p1"))
+            # )
+            # pwField.send_keys(info.password)
 
-            # click sign in button
-            signInBtn = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/section/main/div[1]/div/div/div/div/form/div[3]/button"))
-            )
-            signInBtn.click()
-            print("Signing in")
+            # # click sign in button
+            # signInBtn = WebDriverWait(driver, 10).until(
+            #     EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/section/main/div[1]/div/div/div/div/form/div[3]/button"))
+            # )
+            # signInBtn.click()
+            # print("Signing in")
 
-            # fill in card cvv
-            cvvField = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "credit-card-cvv"))
-            )
-            cvvField.send_keys(info.cvv)
-            print("Attempting to place order")
+            # # fill in card cvv
+            # cvvField = WebDriverWait(driver, 10).until(
+            #     EC.presence_of_element_located((By.ID, "credit-card-cvv"))
+            # )
+            # cvvField.send_keys(info.cvv)
+            # print("Attempting to place order")
 
-            # place order
-            placeOrderBtn = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, ".button__fast-track"))
-            )
-            placeOrderBtn.click()
+            # # place order
+            # placeOrderBtn = WebDriverWait(driver, 10).until(
+            #     EC.presence_of_element_located((By.CSS_SELECTOR, ".button__fast-track"))
+            # )
+            # placeOrderBtn.click()
 
             isComplete = True
         except:
