68e7f0fda19fa6631ec6756fe1d94c863bbbb2fe
alix35301 <alix35301@gmail.com>
2021-11-19 11:15:21 +0500

    added token

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = "743890656:AAF1EqZpfyMA_yPYdC8bGPcv4bFIHpVagZw"
+TOKEN = os.os.environ['TOKEN']
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
